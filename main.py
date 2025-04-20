bfdfddae87d3336c075503a10906413d25e507d7
Ration123 <smartvendingmachinemit@gmail.com>
2025-04-20 17:04:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -159,8 +159,7 @@ elif menu == "🔐 Login / Signup":
             if role == t("User"):
                 st.subheader(t("Card Type: APL"))
                 st.write(t("🧾 Order Status: Not received this month "))
-                a=st.button(t("Place Order"))
-        if a==1:
+                if st.button(t("Place Order")):
                     quantity = st.number_input(t("Enter quantity of rice (in grams)"), min_value=0, step=100)
                     if quantity > 0:
                         price = (quantity / 100) * 10  # ₹10 per 100g
