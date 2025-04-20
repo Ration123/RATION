afa9197d2aa6ff92a81d6cc1948ce6393ae8f36d
Ration123 <smartvendingmachinemit@gmail.com>
2025-04-20 22:23:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -168,12 +168,12 @@ elif menu == "🔐 Login / Signup":
                 with st.form("order_form", clear_on_submit=False):
                   submitted = st.form_submit_button(t("Place Order"))
     
-                if submitted:
-                   quantity = st.number_input(t("Enter quantity of rice (in grams)"), min_value=0, step=100, key="quantity")
-                   if quantity > 0:
-                    price = (quantity / 100) * 10  # ₹10 per 100g
-                    st.write(f"💸 {t('Pay via GPay: UPI@gov')}")
-                    st.success(f"{t('Total Amount')}: ₹{price:.2f}")
+                  if submitted:
+                    quantity = st.number_input(t("Enter quantity of rice (in grams)"), min_value=0, step=100, key="quantity")
+                    if quantity > 0:
+                     price = (quantity / 100) * 10  # ₹10 per 100g
+                     st.write(f"💸 {t('Pay via GPay: UPI@gov')}")
+                     st.success(f"{t('Total Amount')}: ₹{price:.2f}")
 
             
         else:
