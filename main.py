0f72d0311d15884f294636a339a116fe90ec3a4d
Ration123 <smartvendingmachinemit@gmail.com>
2025-04-20 22:58:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -167,7 +167,7 @@ elif menu == "🔐 Login / Signup":
                 st.subheader(t("Card Type: APL"))
                 st.write(t("🧾 Order Status: Not received this month "))
                 with st.form("order_form", clear_on_submit=False):
-                 quantity = st.number_input(t("Enter quantity of rice (in grams)"), min_value=0, step=100, key="quantity")
+                    quantity = st.number_input(t("Enter quantity of rice (in grams)"), min_value=0, step=100, key="quantity")
 
                     # Calculate the price automatically when quantity changes
                     price = (quantity / 100) * 10  # ₹10 per 100g
